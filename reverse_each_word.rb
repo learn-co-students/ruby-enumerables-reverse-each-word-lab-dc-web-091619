350f3ae8a9a0af428c25bb7b15205cef8b5118ee
Charlie Chung <charlieschung@gmail.com>
2019-09-04 18:32:05 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
-#.each
+=begin
+
 def reverse_each_word(string)
   new_array=[]
   array =string.split(" ")
@@ -6,13 +7,28 @@ def reverse_each_word(string)
   new_array.join(" ")
 end
 
-
-
-
-
-#.collect
 def reverse_each_word(string)
   array =string.split(" ")
   reversed_array=array.collect{|n| n.reverse}
   new_string=reversed_array.join(" ")
 end
+=end
+
+#updated
+
+def reverse_each_word(string)
+  new_array=[]
+  string.split(" ").each{|n| new_array<<n.reverse}
+  new_array.join(" ")
+end
+
+def reverse_each_word(string)
+  string.split(" ").collect{|n|n.reverse}.join(" ")
+end
+
+
+
+
+
+
+
