7487f422967fa659bbbb5f4af57600318823533c
github username <github email address>
2019-09-05 18:26:46 -0400

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,22 @@
 require "pry"
-def reverse_each_word (sentence)
-    sentence= "Hello, how are you doing today?"
-    binding.pry
+def reverse_each_word(sentence)
+    # sentence= "Hello, how are you doing today?"
+
+    words= sentence.split(" ")
+
+    # words.collect do |word|
+    #     word.reverse
+    # end.join(' ')
+    # binding.pry
+
+    # THE ABOVE CODE FROM LINE 7 to LINE 9 WORKS, But the method below works
+
+    updated_words = words.collect do |word| 
+        word.reverse
+    end
+    updated_words.join(' ')
+     
+
+     
 end
+# p reverse_each_word("Hello, how are you doing today?")
